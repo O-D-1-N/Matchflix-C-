5960327dbba3a2e7ac06375a61782dc8c5feca78
Odin <64448868+O-D-1-N@users.noreply.github.com>
2020-11-08 00:15:44 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace Matchflix__GITHUB_ {
+    class Program {
+
+        public static async Task Main() {
+
+            string movieid = "81043135";
+            var matchflix = new Matchflix(movieid);
+
+            // You must call `getdetails` and `getcountries` before writing info. //
+
+            await matchflix.GetDetailsAysnc();
+            await matchflix.GetCountriesAsync();
+
+            Console.WriteLine(matchflix.Title);
+            // Output = "Hot Gimmick: Girl Meets Boy" //
+        }
+
+    }
+}
